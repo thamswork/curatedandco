357ebe17a839f629b299ba9e7ea4d3c9c6ad776e
thamswork <tyme.sak@gmail.com>
2026-04-25 12:14:35 +0700

    fix: switch to static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   }
